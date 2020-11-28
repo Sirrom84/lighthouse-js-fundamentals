516b50682d44b067828a333d3d5846da9f0d20ce
sirrom84 <morrisrjc@gmail.com>
2020-11-28 18:10:52 +0000

    Made the odd/even checker

---
+++ odd.js
@@ -0,0 +1,11 @@
+function isOdd(num) {
+  if (num % 2 === 0) {
+    return num + " is an even number!"
+  }else {
+    return num + " is an odd number!"
+  }
+  
+  
+}
+console.log(isOdd(3));
+console.log(isOdd(8));
